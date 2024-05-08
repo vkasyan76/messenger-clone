57d73f92c0423950311b08f24333909f6557e275
Valentyn Kasyan <vkasyan@gmx.de>
2024-05-08 16:31:31 +0200

    Login Functionality and Social Login

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,10 +7,15 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import { toast } from "react-hot-toast";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
+  // const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -36,42 +41,42 @@ const AuthForm = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
-      //     .then(() => signIn("credentials", data))
-      //     .catch(() => toast.error("Something went wrong!"))
-      //     .finally(() => setIsLoading(false));
+      axios
+        .post("/api/register", data)
+        .then(() => signIn("credentials", data))
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
-      //   signIn("credentials", {
-      //     ...data,
-      //     redirect: false,
-      //   })
-      //     .then((callback) => {
-      //       if (callback?.error) {
-      //         toast.error("Invalid credentials");
-      //       }
-      //       if (callback?.ok && !callback?.error) {
-      //         toast.success("Logged in!");
-      //         router.push("/users");
-      //       }
-      //     })
-      //     .finally(() => setIsLoading(false));
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+            // router.push("/users");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
   const socialAction = (action: string) => {
     setIsLoading(true);
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials");
+        }
 
-    //   signIn(action, { redirect: false })
-    //     .then((callback) => {
-    //       if (callback?.error) {
-    //         toast.error("Invalid Credentials");
-    //       }
-
-    //       if (callback?.ok && !callback?.error) {
-    //         toast.success("Logged in!");
-    //       }
-    //     })
-    //     .finally(() => setIsLoading(false));
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
