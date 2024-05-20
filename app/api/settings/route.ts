d90d3d214c0a6646cdfce4545c08a0ac2552db9e
Valentyn Kasyan <vkasyan@gmx.de>
2024-05-20 15:41:31 +0200

    Settings Modal and Settings API route

---
+++ app/api/settings/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+
+    // destructure the body:
+
+    const body = await request.json();
+
+    const { name, image } = body;
+
+    // check for currrent user
+    if (!currentUser?.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // update the user
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        image: image,
+        name: name,
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error: any) {
+    console.log(error, "ERROR_SETTINGS");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
