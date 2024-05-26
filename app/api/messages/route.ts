aeb0db7a6e47f56d7676ce15aa4944e68f81bd42
Valentyn Kasyan <vkasyan@gmx.de>
2024-05-26 18:51:35 +0200

    Real Time Messages with pusher

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,8 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
+import { pusherServer } from "@/app/libs/pusher";
+
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser();
@@ -55,6 +57,21 @@ export async function POST(request: Request) {
         },
       },
     });
+
+    // set-up for pusher:
+    await pusherServer.trigger(conversationId, "messages:new", newMessage);
+
+    // extract last message from the conversation
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1];
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, "conversation:update", {
+        id: conversationId,
+        messages: [lastMessage],
+      });
+    });
+
     return NextResponse.json(newMessage);
   } catch (error: any) {
     console.log(error, "ERROR_MESSAGES");
